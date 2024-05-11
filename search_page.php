c36e60bb6bec5aa4af77438b40a033a7cdddb69f
SandunLB <sandunlb2001@gmail.com>
2024-05-11 17:31:13 +0530

    search page css styles created!

--- search_page.php
+++ search_page.php
@@ -63,72 +63,65 @@ if(isset($_POST['add_to_cart'])){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>search page</title>
-
-  
-   <link rel="stylesheet" href="css/style.css">
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search Page</title>
+    <link rel="stylesheet" href="css/search.css">
 </head>
 <body>
-   
-<?php @include 'header.php'; ?>
+    <?php @include 'header.php'; ?>
 
+    <div class="container">
+        <section class="search-section">
+            <div class="search-container">
+                <h2>Search Products</h2>
+                <form action="" method="POST" class="search-form">
+                    <input type="text" class="search-box" placeholder="Search products..." name="search_box">
+                    <input type="submit" class="search-btn" value="Search" name="search_btn">
+                </form>
+            </div>
+        </section>
 
-<section class="search-form">
-    <form action="" method="POST">
-        <input type="text" class="box" placeholder="search products..." name="search_box">
-        <input type="submit" class="btn" value="search" name="search_btn">
-    </form>
-</section>
+        <section class="products-section">
+            <div class="products-container">
+                <?php
+                if(isset($_POST['search_btn'])){
+                    $search_box = mysqli_real_escape_string($conn, $_POST['search_box']);
+                    $select_products = mysqli_query($conn, "SELECT * FROM `products` WHERE name LIKE '%{$search_box}%'") or die('query failed');
+                    if(mysqli_num_rows($select_products) > 0){
+                        while($fetch_products = mysqli_fetch_assoc($select_products)){
+                ?>
+                <div class="product-box">
+                    <form action="" method="POST" class="product-form">
+                        <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="view-btn"><i class="fas fa-eye"></i></a>
+                        <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
+                        <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="product-image">
+                        <div class="name"><?php echo $fetch_products['name']; ?></div>
+                        <input type="number" name="product_quantity" value="1" min="0" class="qty">
+                        <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
+                        <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
+                        <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
+                        <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
+                        <input type="submit" value="Add to Wishlist" name="add_to_wishlist" class="wishlist-btn">
+                        <input type="submit" value="Add to Cart" name="add_to_cart" class="cart-btn">
+                    </form>
+                </div>
+                <?php
+                        }
+                    } else {
+                        echo '<p class="empty">No results found!</p>';
+                    }
+                } else {
+                    echo '<p class="empty">Search something!</p>';
+                }
+                ?>
+            </div>
+        </section>
+    </div>
 
-<section class="products" style="padding-top: 0;">
+    <?php @include 'footer.php'; ?>
 
-   <div class="box-container">
-
-      <?php
-        if(isset($_POST['search_btn'])){
-         $search_box = mysqli_real_escape_string($conn, $_POST['search_box']);
-         $select_products = mysqli_query($conn, "SELECT * FROM `products` WHERE name LIKE '%{$search_box}%'") or die('query failed');
-         if(mysqli_num_rows($select_products) > 0){
-            while($fetch_products = mysqli_fetch_assoc($select_products)){
-      ?>
-      <form action="" method="POST" class="box">
-         <a href="view_page.php?pid=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
-         <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
-         <img src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="image">
-         <div class="name"><?php echo $fetch_products['name']; ?></div>
-         <input type="number" name="product_quantity" value="1" min="0" class="qty">
-         <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
-         <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-         <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
-         <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-         <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
-         <input type="submit" value="add to cart" name="add_to_cart" class="btn">
-      </form>
-      <?php
-         }
-            }else{
-                echo '<p class="empty">no result found!</p>';
-            }
-        }else{
-            echo '<p class="empty">search something!</p>';
-        }
-      ?>
-
-   </div>
-
-</section>
-
-
-
-
-
-<?php @include 'footer.php'; ?>
-
-<script src="js/script.js"></script>
-
+    <script src="js/script.js"></script>
 </body>
 </html>
